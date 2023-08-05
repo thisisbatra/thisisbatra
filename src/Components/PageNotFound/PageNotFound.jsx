f6e1a7d15f2b5ca866657f8cb7f8a333bf17af61
thisisbatra <batranitin118@gmail.com>
2023-08-06 00:43:56 +0530

    loader animation completed!

--- src/Components/PageNotFound/PageNotFound.jsx
+++ src/Components/PageNotFound/PageNotFound.jsx
@@ -9,9 +9,9 @@ const PageNotFound = () => {
     return (
         <>
             <section className={Styles.section}>
-                <div>
-                    <h1>404</h1>
-                    <p>I am sorry, but the page you are requested was not found :(</p>
+                <div className={Styles.div}>
+                    <h1 className={Styles.heading}>404</h1>
+                    <p className={Styles.para}>I am sorry, but the page you are requested was not found :(</p>
                     <button onClick={onRedirectclick} className={Styles.button}>Redirect</button>
                 </div>
             </section>
