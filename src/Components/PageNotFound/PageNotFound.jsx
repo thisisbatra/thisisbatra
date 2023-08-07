7ecfb0ef1542f9043b7494e87704497841cfea5e
thisisbatra <batranitin118@gmail.com>
2023-08-08 02:22:31 +0530

    working on pagenotfound

--- src/Components/PageNotFound/PageNotFound.jsx
+++ src/Components/PageNotFound/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Styles from "./PAgeNotFound.module.css"
+import Styles from "./PageNotFound.module.css"
 const PageNotFound = () => {
     const nav=useNavigate()
     const onRedirectclick=()=>{
@@ -11,8 +11,12 @@ const PageNotFound = () => {
             <section className={Styles.section}>
                 <div className={Styles.div}>
                     <h1 className={Styles.heading}>404</h1>
-                    <p className={Styles.para}>I am sorry, but the page you are requested was not found :(</p>
-                    <button onClick={onRedirectclick} className={Styles.button}>Redirect</button>
+                    <p className={Styles.para}>I am sorry, but the page you have requested was not found :(</p>
+                    <div className={Styles.buttonBox}>
+                        <button onClick={onRedirectclick} className={Styles.button}>Redirect</button>
+                        <span className={Styles.buttonCase}></span>
+                    </div>
+                    
                 </div>
             </section>
         </>
