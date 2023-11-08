937ac7a8de866ee27d7cb6117cd5af31bea4e32f
thisisbatra <batranitin118@gmail.com>
2023-11-08 11:08:02 +0530

    setting landing page

--- src/Components/VersionOne/NavBar/NavBar.jsx
+++ src/Components/VersionOne/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
+import styles from './navBeauty.module.css';
 const NavBar = () => {
     let navigate=useNavigate();
     let changeRouteViaOption=(e)=>{
@@ -8,17 +8,19 @@ const NavBar = () => {
         value==="v1"?navigate('/'):navigate('/comingSoon');
     }
     return (
-        <div>
-            <Link to='/home'><i className="bi bi-house-door"></i></Link>
-            <Link to='/about'><i className="bi bi-info-circle"></i></Link>
-            <Link to='/education'><i className="bi bi-mortarboard"></i></Link>
-            <Link to='/skills'><i className="bi bi-pencil-square"></i></Link>
-            <Link to='/contact'><i className="bi bi-person-lines-fill"></i></Link>
-            <select name="" id="" onChange={changeRouteViaOption}>
-                <option value="0">v1</option>
-                <option>v2</option>
-            </select>
-        </div>
+        <ul className={styles.con}>
+            <li><Link to='/home'><i className="bi bi-house-door"></i></Link></li>
+            <li><Link to='/about'><i className="bi bi-info-circle"></i></Link></li>
+            <li><Link to='/education'><i className="bi bi-mortarboard"></i></Link></li>
+            <li><Link to='/skills'><i className="bi bi-pencil-square"></i></Link></li>
+            <li><Link to='/contact'><i className="bi bi-person-lines-fill"></i></Link></li>
+            <li>
+                <select name="" id="" onChange={changeRouteViaOption}>
+                    <option value="0">v1</option>
+                    <option>v2</option>
+                </select>
+            </li>
+        </ul>
     );
 }
 
--- src/Components/VersionOne/VersionOne.jsx
+++ src/Components/VersionOne/VersionOne.jsx
@@ -5,10 +5,44 @@ import { Outlet } from 'react-router-dom';
 const VersionOne = () => {
     return (
         <>
-            <NavBar/>
+            <nav>
+                <ThemeChanger/>
+                <NavBar/>
+                <ThumbNail/>
+            </nav>
             <Outlet/>
+            <footer>
+                <FollowPalette/>
+                <h1>this website is developed & maintained by Nitin Batra</h1>
+            </footer>
         </>
     );
+}
+
+const ThemeChanger=()=>{
+    return(
+        <i className="bi bi-brightness-high"></i>
+    )
+}
+
+const ThumbNail=()=>{
+    return(
+        <div>
+            <img src="../assets/meThumb.png" alt="thumbnail"/>
+            <h1>thisisbatra</h1>
+        </div>
+    )
+}
+
+const FollowPalette=()=>{
+    return(
+        <div>
+            <div><i className="bi bi-instagram"></i></div>
+            <div><i className="bi bi-linkedin"></i></div>
+            <div><i className="bi bi-github"></i></div>
+            <div><i className="bi bi-code-slash"></i></div>
+        </div>
+    )
 }
 
 export default VersionOne;
