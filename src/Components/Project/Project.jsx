d6de17c5454cd9b58ab354d1e3062c9c9e34cf65
thisisbatra <batranitin118@gmail.com>
2024-12-30 19:50:22 +0530

    project component is completed

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,34 +1,77 @@
-import React from 'react';
+import React, { useRef,useState } from 'react';
 import style from './Project.module.css';
-import { FaEye,FaCode } from "react-icons/fa";
+import { FaEye, FaCode } from "react-icons/fa";
 import { SiReadme } from "react-icons/si";
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle  } from "react-icons/io";
+import { NavLink } from 'react-router-dom';
 
 const Project = () => {
-    let projectData=[{
+    const forScrollRef=useRef(null);
+    const [canScrollLeft, setCanScrollLeft] = useState(false);
+    const [canScrollRight, setCanScrollRight] = useState(true);
+
+    const handleScroll=()=>{
+        // console.log(forScrollRef);
+        let scroller=forScrollRef.current;
+        if(!scroller) return;
+
+        const { scrollLeft, scrollWidth, clientWidth } = scroller;
+        setCanScrollLeft(scrollLeft > 0);
+        setCanScrollRight(scrollLeft + clientWidth < scrollWidth);
+    }
+
+    const scrollLeft = () => {
+        forScrollRef.current.scrollBy({ left: -200, behavior: "smooth" });
+      };
+    
+      const scrollRight = () => {
+        forScrollRef.current.scrollBy({ left: 200, behavior: "smooth" });
+      };
+
+    const projectData=[{
         id: 1,
-        title:"chorus",
+        title:"title",
         preview:true,
-        techUsed:["react","springboot","postgres"],
+        techUsed:["name1","name2","name3"],
         source:true,
         blog:true
     },{
         id: 2,
+        title:"title",
+        preview:true,
+        techUsed:["name1","name2"],
+        source:true,
+        blog:true
     },{
         id: 3,
+        title:"title",
+        preview:true,
+        techUsed:["name1"],
+        source:true,
+        blog:true
     },{
         id: 4,
+        title:"title",
+        preview:true,
+        techUsed:["name1","name2","name3"],
+        source:true,
+        blog:true
     }]
     return (
         <section className={style.section}>
-            <div className={style.cartHolder}>
+            <div className={style.scrollBar}>
+                <IoIosArrowDropleftCircle className={canScrollLeft===true?style.arrow:style.arrowDisabled} onClick={scrollLeft}/>
+                <IoIosArrowDroprightCircle className={canScrollRight===true?style.arrow:style.arrowDisabled} onClick={scrollRight}/>
+            </div>
+            <div className={style.cartHolder} ref={forScrollRef} onScroll={handleScroll}>
                 {
-                projectData.map((i)=><ProjectCart key={i.id}/>)
+                projectData.map((i)=><ProjectCart key={i.id} data={i}/>)
                 }
             </div>
         </section>
     );
 }
-const ProjectCart=()=>{
+const ProjectCart=(props)=>{
     return(
         <div className={style.ProjectCart}>
             <div className={style.ProjectPrev}>
@@ -38,18 +81,18 @@ const ProjectCart=()=>{
                 </span>
             </div>
             <div className={style.ProjectDesc}>
-                <h1>title</h1>
-                <span>
-                    tech used
+                <h1>{props.data.title}</h1>
+                <span className={style.forList}>
+                    <h4>tech used</h4>
                     <ul>
-                        <li>name1</li>
-                        <li>name2</li>
-                        <li>name3</li>
+                        {props.data.techUsed.map((i,index)=>
+                            <li key={index}>{i}</li>
+                            )}
                     </ul>
                 </span>
-                <span>
-                    <a href="#"><FaCode/>source</a>
-                    <a href="#"><SiReadme/>blog</a>
+                <span className={style.forBtn}>
+                    <NavLink to="#"><FaCode/>source</NavLink>
+                    <NavLink to="#"><SiReadme/>blog</NavLink>
                 </span>
             </div>
         </div>
